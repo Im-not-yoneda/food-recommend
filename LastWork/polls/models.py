84ade8d88d8d168a1a6af8a844bd06a07ec2f930
米田匠吾 <yoneda.shogo@gmail.com>
2023-12-07 22:45:19 +0900

    DBを使用できるように変更しました。

--- LastWork/polls/models.py
+++ LastWork/polls/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class food(models.Model):
+    name = models.CharField(max_length=100)
+    calorie = models.IntegerField()
+    value = models.IntegerField()
