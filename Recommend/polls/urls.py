6b669a87af38cd664538d59e45d433d1848a1ff3
米田匠吾 <yoneda.shogo@gmail.com>
2023-12-09 17:41:56 +0900

    TOPページのデザインを変更しました。

--- Recommend/polls/urls.py
+++ Recommend/polls/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.foodrecommend, name="knapsac"),
+    path("", views.foodrecommend, name="recommend"),
     path("about/", views.about, name="about"),
     path("insert/", views.insertFood, name="insert")
 ]
