214ba9b047da38809db5f933bec292588b52e37e
米田匠吾 <yoneda.shogo@gmail.com>
2023-12-18 00:43:10 +0900

    食材をデータベースから表示するように変更しました。

--- Recommend/polls/models.py
+++ Recommend/polls/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 # Create your models here.
 class food(models.Model):
-    name = models.CharField(max_length=100)
-    calorie = models.IntegerField()
-    value = models.IntegerField()
+    name = models.CharField('name',max_length=100)
+    calorie = models.IntegerField('calorie')
+    value = models.IntegerField('value')
--- Recommend/polls/views.py
+++ Recommend/polls/views.py
@@ -3,18 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from .forms import CheckBox
-from .forms import NumberInput
+from .forms import test_form
 from pulp import LpProblem, LpVariable, LpMaximize
 from .models import food
 
 def foodrecommend(request):
-    weights = []
-    values = []
-    name = []
-    result = None
-    total_weight = None
-    select_name = None
-
     def knapsack_solver(weights, values, capacity):
         num_items = len(weights)
         # 合計価値を最大化するための線形プログラム問題を作成
@@ -34,83 +27,12 @@ def foodrecommend(request):
             'total_value': sum(float(values[i]) for i in selected_items),
             'total_weight': sum(float(weights[i]) for i in selected_items)
         }
-
-
+    
     if request.method == 'POST':
-        formNum = NumberInput(request.POST)
-        if formNum.is_valid():
-            # フォームのデータを処理
-            number = formNum.cleaned_data['number']
-            result = number
+        checkbox = CheckBox(request.POST)
     else:
-        formNum = NumberInput()
-
-    if request.method == 'POST':
-        formBox = CheckBox(request.POST)
-        # 選択した要素一覧
-        if formBox.is_valid():
-            formBox_reg = formBox.cleaned_data.get('chickin_reg')
-            formBox_chest = formBox.cleaned_data.get('chickin_chest')
-            formBox_brrocori = formBox.cleaned_data.get('brrocori')
-            formBox_carrot = formBox.cleaned_data.get('carrot')
-            formBox_onion = formBox.cleaned_data.get('onion')
-            formBox_beef = formBox.cleaned_data.get('beef')
-            formBox_pork = formBox.cleaned_data.get('pork')
-            formBox_rice = formBox.cleaned_data.get('rice')
-            formBox_potato = formBox.cleaned_data.get('potato')
-            selected_options = [formBox.fields[key].label for key, value in formBox.cleaned_data.items() if value]
-            # タンパク質とカロリーを定義
-            if formBox_reg:
-                values.append(25)
-                weights.append(200)
-                name.append("鶏もも肉")
-            if formBox_chest:
-                values.append(23)
-                weights.append(150)
-                name.append("鶏むね肉")
-            if formBox_brrocori:
-                values.append(4)
-                weights.append(50)
-                name.append("ブロッコリー")
-            if formBox_carrot:
-                values.append(1)
-                weights.append(28)
-                name.append("にんじん")
-            if formBox_onion:
-                values.append(1)
-                weights.append(33)
-                name.append("たまねぎ")
-            if formBox_beef:
-                values.append(20)
-                weights.append(148)
-                name.append("牛もも肉")
-            if formBox_pork:
-                values.append(14)
-                weights.append(386)
-                name.append("豚バラ肉")
-            if formBox_rice:
-                values.append(1000)
-                weights.append(168)
-                name.append("ごはん")
-            if formBox_potato:
-                values.append(3)
-                weights.append(59)
-                name.append("じゃがいも")
-            knapsack_result = knapsack_solver(weights, values, number)
-            selected_items = knapsack_result['selected_items']
-            total_value = sum(values)
-            if total_value >= 1000:
-                total_value = total_value - 998
-            total_weight = knapsack_result['total_weight']
-            select_name = [name[i] for i in selected_items]
-        else:
-            selected_options = []
-            return render(request, 'polls/index.html', {'formNum': formNum, 'result': result,'weights': weights,'option':name, 'values':values, 'formBox': formBox, 'selected_options': selected_options, 'calory': total_weight, 'name':select_name, 'value': total_value})
-    else:
-        formBox = CheckBox()
-        selected_options = []
-        return render(request, 'polls/index.html', {'formNum': formNum, 'result': result,'weights': weights,'option':name, 'values':values, 'formBox': formBox, 'selected_options': selected_options, 'calory': total_weight, 'name':select_name})
-    return render(request, 'polls/result.html', {'formNum': formNum, 'result': result,'weights': str(weights)[1:-1],'option':",".join(name), 'values':str(values)[1:-1], 'formBox': formBox, 'selected_options': selected_options, 'calory': total_weight, 'name':",".join(select_name), 'value': total_value})
+        checkbox = CheckBox()
+    return render(request, 'polls/index.html', {'food_names': checkbox})
 
 def insertFood(request):
     return render(request, 'polls/insertFood.html')
@@ -119,7 +41,8 @@ def about(request):
     return render(request, 'polls/about.html')
 
 def test(request):
-    context = {
-        'db_list': food.objects.all()
-    }
-    return render(request, 'polls/test.html', context)
+    if request.method == 'POST':
+        form = test_form(request.POST)
+    else:
+        form = test_form()
+    return render(request, 'polls/test.html', {'db_list': food.objects.all(),'food_name': form})
