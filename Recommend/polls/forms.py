64808d084e2b92fd77868ccc94f037cc6a5d3618
米田匠吾 <yoneda.shogo@gmail.com>
2023-12-20 23:27:48 +0900

    データを挿入するフォームを追加しました。

--- Recommend/polls/forms.py
+++ Recommend/polls/forms.py
@@ -9,12 +9,21 @@ class CheckBox(forms.Form):
         required = False
     )
 
+class calorie_form(forms.Form):
+    calorie = forms.DecimalField(label='目標摂取カロリー', max_digits=10000, decimal_places=0)
+
+class insert_name(forms.Form):
+    name = forms.CharField(label='name',max_length=100)
+
+class insert_calorie(forms.Form):
+    calorie = forms.IntegerField(label='calorie')
+
+class insert_value(forms.Form):
+    value = forms.IntegerField(label='value')
+
 class test_form(forms.Form):
     food_name = forms.MultipleChoiceField(
         choices = food.objects.values_list('name','name').distinct(),
         widget = forms.CheckboxSelectMultiple,
         required = False
     )
-
-class calorie_form(forms.Form):
-    calorie = forms.DecimalField(label='目標摂取カロリー', max_digits=10000, decimal_places=0)
