95e235d413c85e9bc52502a6db9c392c36d253e1
米田匠吾 <yoneda.shogo@gmail.com>
2024-01-07 19:44:26 +0900

    フォルダの削除

--- src/Recommend/polls/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-
-# Create your models here.
-class food(models.Model):
-    name = models.CharField('name',max_length=100)
-    calorie = models.IntegerField('calorie')
-    value = models.IntegerField('value')
-    user_name = models.ForeignKey(User, on_delete=models.CASCADE)
